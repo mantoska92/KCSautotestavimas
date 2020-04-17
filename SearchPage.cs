8441bd4eb63e7dd1a0f19e7b722d68b301694f0e
mantoska92 <44406019+mantoska92@users.noreply.github.com>
2020-04-17 18:34:46 +0300

    Add files via upload

---
+++ SearchPage.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using baigiamasis.Tests;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baigiamasis.Pages
+{
+   public class SearchPage : BasePage
+    {
+        public SearchPage(IWebDriver driver) : base(driver) { }
+
+        private IWebElement detailSearchElement => Driver.FindElement(By.Id("detail_search_link"));
+        private IWebElement carMakeElement => Driver.FindElement(By.Name("make_id[]"));
+        private IWebElement chosenMake => Driver.FindElement(By.LinkText("BMW"));
+        private IWebElement carModelElement => Driver.FindElement(By.Name("model_id[]"));
+        private IWebElement chosenModel => Driver.FindElement(By.CssSelector(".open .category-dropdown-item:nth-child(39) > a"));
+        private IWebElement submitSearchElement => Driver.FindElement(By.CssSelector(".detail-rezult-saveButtons:nth-child(5) > .showSearchRezult"));
+
+        
+      
+        public void ClickDetailSearchElement()
+        {
+            detailSearchElement.Click();
+        }
+        
+        public void ClickCarMakeElement()
+        {
+            carMakeElement.Click();
+        }
+        public void ClickChosenMakelElement()
+        {
+            chosenMake.Click();
+        }
+        public void ClickCarModelElement()
+        {
+            carModelElement.Click();
+        }
+        public void ClickChosenModelElement()
+        {
+            chosenModel.Click();
+        }
+        public void ClickSubmitSearch()
+        {
+            submitSearchElement.Click();
+            
+        }
+
+       public void AssertCarMakeElement()
+        {
+            Assert.IsNotNull(chosenMake);
+        }
+
+        
+        
+        
+       
+    }
+}
---
+++ SearchTest.cs
@@ -0,0 +1,36 @@
+﻿using baigiamasis.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baigiamasis.Tests
+{
+   public class SearchTest : BaseTest
+    {
+        
+        private SearchPage searchPage;
+
+        [SetUp]
+        public void InitPages()
+        {
+            loginPage = new LoginPage(Driver);
+            homePage = new HomePage(Driver);
+            searchPage = new SearchPage(Driver);
+            
+        }
+        [Test]
+
+        public void SearchingTest()
+        {
+            searchPage.ClickDetailSearchElement();
+            searchPage.ClickCarMakeElement();
+            searchPage.ClickChosenMakelElement();
+            searchPage.ClickCarModelElement();
+            searchPage.ClickChosenModelElement();
+            searchPage.ClickSubmitSearch();
+
+            searchPage.AssertCarMakeElement();
+        }
+    }
+}
